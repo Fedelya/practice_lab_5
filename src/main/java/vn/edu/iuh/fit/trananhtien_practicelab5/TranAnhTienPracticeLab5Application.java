d728c9f2c872df041e4f338faad87bb33df141d3
Fedelya <100048385+Fedelya@users.noreply.github.com>
2024-12-12 12:25:11 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/fit/trananhtien_practicelab5/TranAnhTienPracticeLab5Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.trananhtien_practicelab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranAnhTienPracticeLab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranAnhTienPracticeLab5Application.class, args);
+    }
+
+}
