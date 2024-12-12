581552c9218e3611328756d80e1feef6d3c0b40e
Fedelya <100048385+Fedelya@users.noreply.github.com>
2024-12-13 01:09:23 +0700

    them trang update-candidate

--- src/main/java/vn/edu/iuh/fit/trananhtien_practicelab5/frontend/controllers/CandidateController.java
+++ src/main/java/vn/edu/iuh/fit/trananhtien_practicelab5/frontend/controllers/CandidateController.java
@@ -1,13 +1,17 @@
 package vn.edu.iuh.fit.trananhtien_practicelab5.frontend.controllers;
 
+import com.neovisionaries.i18n.CountryCode;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import vn.edu.iuh.fit.trananhtien_practicelab5.backend.models.Address;
 import vn.edu.iuh.fit.trananhtien_practicelab5.backend.models.Candidate;
+import vn.edu.iuh.fit.trananhtien_practicelab5.backend.repositories.AddressRepository;
 import vn.edu.iuh.fit.trananhtien_practicelab5.backend.repositories.CandidateRepository;
 import vn.edu.iuh.fit.trananhtien_practicelab5.backend.services.CandidateService;
 
@@ -22,6 +26,8 @@ public class CandidateController {
     private CandidateRepository candidateRepository;
     @Autowired
     private CandidateService candidateService;
+    @Autowired
+    private AddressRepository addressRepository;
 
     @GetMapping("/list")
     public String showCandidateList(Model model) {
@@ -47,4 +53,31 @@ public class CandidateController {
         }
         return "candidates/candidates-paging";
     }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") long id){
+        ModelAndView modelAndView = new ModelAndView();
+        Optional<Candidate> opt = candidateRepository.findById(id);
+        if(opt.isPresent()){
+            Candidate candidate = opt.get();
+            modelAndView.addObject("candidate", candidate);
+            modelAndView.addObject("address", candidate.getAddress());
+            modelAndView.addObject("countries", CountryCode.values());
+            modelAndView.setViewName("candidates/update-candidates");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/candidates/update-candidates")
+    public String updateCandidate(
+            @ModelAttribute("candidate") Candidate candidate,
+            @ModelAttribute("address") Address address,
+            BindingResult result, Model model
+    ){
+        addressRepository.save(address);
+        candidate.setAddress(address);
+        candidateRepository.save(candidate);
+        return "redirect:/candidates";
+    }
+
 }
